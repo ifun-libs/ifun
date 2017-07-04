85ef06161c9c8fa5f4048f10da18e906cc7798b8
利开团 <likaituan@wecash.net>
2017-07-04 16:28:00 +0800

    quick submit

--- ua.js
+++ ua.js
@@ -13,6 +13,11 @@ Mac浏览器示例:
 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
  */
 
+/*
+Android 浏览器示例:
+ Mozilla/5.0 (Linux; Android 7.1.1; MI 6 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043307 Safari/537.36 MicroMessenger/6.5.8.1060 NetType/WIFI Language/zh_CN
+ */
+
 
 // 选择符合条件的一个
 var filterItem = function (ua, ...args) {
